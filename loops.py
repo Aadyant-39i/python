cceebad4f3fd074c5b7fdea0b561fea1e7b0d613
Aadyant-39i <nmadhupnb@gmail.com>
2026-06-30 17:31:27 +0530

    Add files via upload

--- loops.py
+++ loops.py
@@ -1,3 +1,31 @@
 x = int(input("Enter a number: "))
 for i in range(1, 11):
     print(f"{x} x {i}={x * i}")
+
+n = int(input("Enter the number of rows: "))
+for i in range(1, n+1):
+    for j in range(i):
+        print("★", end=" ")
+    print()
+
+total_sum = 0
+num = 1
+while num <= 10:
+    total_sum += num
+    num += 1
+print(f"    the sum of the first 10 natural numbers is: {total_sum}")
+
+
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i ==0:
+            print(f"{num} is not a prime number")
+            break
+        else:
+            print(f"{num} is a prime number")
+else:
+     print(f"{num} is not a prime number")
+    
+    
+
